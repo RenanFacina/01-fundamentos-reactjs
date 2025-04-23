8a13e3bb39a160411b718aa4fe01ad0b2d456192
Renan Facina <62966455+RenanFacina@users.noreply.github.com>
2025-04-23 17:01:28 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
+
+import styles from "./App.module.css";
+
+import "./global.css";
+
+const posts = [
+    {
+        id: 1,
+        author: {
+            avatarUrl: 'https://github.com/RenanFacina.png',
+            name: 'Renan Facina',
+            role: 'Developer'
+        },
+        content: [
+            { type: 'paragraph', content: 'Lorem ipsum dolor sit amet.'},
+            { type: 'paragraph', content: 'Lorem, ipsum dolor sit consectetur adipisicing elit. Hic, quas.'},
+            { type: 'link', content: 'jane/design/doctorcare'}
+        ],
+        publishedAt: new Date('2024-04-27 20:00:00'),
+    },
+    {
+        id: 2,
+        author: {
+            avatarUrl: 'https://github.com/diego3g.png',
+            name: 'Diego Fernandes',
+            role: '@Rocketseat'
+        },
+        content: [
+            { type: 'paragraph', content: 'Lorem ipsum dolor sit amet.'},
+            { type: 'paragraph', content: 'Lorem, ipsum dolor sit consectetur adipisicing elit. Hic, quas.'},
+            { type: 'link', content: 'jane/design/doctorcare'}
+        ],
+        publishedAt: new Date('2024-04-30 10:00:00'),
+    },
+];
+
+export function App() {
+    return (
+        <div>
+            <Header />
+
+            <div className={styles.wrapper}>
+                <Sidebar />
+                <main>
+                    {posts.map(post => {
+                        return (
+                            <Post
+                                key={post.id}
+                                author={post.author}
+                                content={post.content}
+                                publishedAt={post.publishedAt}
+                            />
+                        )
+                    })}
+                </main>
+            </div>
+        </div>
+    );
+}
